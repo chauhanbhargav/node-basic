88cfe7030fb6cb13f6bb589302b878c94abfafec
Bhargav <bhargavchauhan777@hotmail.com>
2017-11-04 19:15:53 +0530

    Example of Http request

---
+++ basic-http-request/app.js
@@ -0,0 +1,29 @@
+// importing modules
+const express = require("express");
+
+// app is a instance of express 
+// to run express on top of nodejs need to run app instance 
+const app = express();
+
+// basic http request start // 
+app.get('/get', function(req, res) {
+    res.send("Hello World!");
+});
+
+app.post('/post', function(req, res) {
+    res.send("Catch POST Request");
+});
+
+app.put('/put', function(req, res) {
+    res.send("Catch PUT Request");
+});
+
+app.delete('/delete', function(req, res) {
+    res.send("Catch DELETE Request");
+});
+// basic http request end //
+
+// server port listen on 3000
+app.listen(8080, function() {
+    console.log('Server is running on port 8080');
+});
