c745db2cc118aa74cd924fa8ad2f449b5e062bbc
Bhargav <bhargavchauhan777@hotmail.com>
2017-11-05 10:58:02 +0530

    -- Express routing

---
+++ Express-routing/app.js
@@ -0,0 +1,17 @@
+// importing modules
+const express = require("express");
+const route = require("./route");
+
+// app is a instance of express 
+// to run express on top of nodejs need to run app instance 
+const app = express();
+
+
+// load homeRoute module
+app.use(route);
+
+
+// server port listen on 3000
+app.listen(8080, function() {
+    console.log('Server is running on port 8080');
+});
